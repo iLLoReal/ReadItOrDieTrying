bc113960a72da6a7492df7a8771be7f78be3a8b5
iLLoReal <olivier.laffon.dev@gmail.com>
2025-11-23 15:56:01 +0100

    temp push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import {Header} from "./components/header";
-import {Reader} from "./components/Reader";
-import {useState} from "react";
+import { Header } from "./components/header";
+import { Reader } from "./components/Reader";
+import { useState } from "react";
 
 export const App = () => {
   const [currentText, setCurrentText] = useState('');
@@ -8,56 +8,56 @@ export const App = () => {
   const map = { slowest: 10, reallySlow: 20, slow: 30, medium: 40, fast: 60, reallyFast: 70, fastest: 80 };
   const [timeBetweenReads, setTimebetweenReads] = useState(map.slowest);
   const entry = Object.entries(map).find(
-      ([, val]) => val === timeBetweenReads
+    ([, val]) => val === timeBetweenReads
   );
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-around'
-      }}>
-        <div>
-      <Header />
-          <select onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-            const value = map[e.target.value as keyof typeof map];
-            if (value) setTimebetweenReads(value)
-          }}>
-            <option value={'slowest'}>the slowest</option>
-            <option value={'reallySlow'}>really slow</option>
-            <option value={'slow'}>slow</option>
-            <option value={'medium'}>normal</option>
-            <option value={'fast'}>fast</option>
-            <option value={'reallyFast'}>really fast</option>
-            <option value={'fastest'}>the fastest</option>
-          </select>
-        </div>
-        <div>
-          <textarea onBlur={(e) => setCurrentText(() => e.target.value)}>
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+    }}>
+      <div>
+        <Header headerTitle="toto" />
+        <select onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+          const value = map[e.target.value as keyof typeof map];
+          if (value) setTimebetweenReads(value)
+        }}>
+          <option value={'slowest'}>the slowest</option>
+          <option value={'reallySlow'}>really slow</option>
+          <option value={'slow'}>slow</option>
+          <option value={'medium'}>normal</option>
+          <option value={'fast'}>fast</option>
+          <option value={'reallyFast'}>really fast</option>
+          <option value={'fastest'}>the fastest</option>
+        </select>
+      </div>
+      <div>
+        <textarea onBlur={(e) => setCurrentText(() => e.target.value)}>
 
-          </textarea>
+        </textarea>
 
-          <button
-            type={'button'}
-            onClick={() => setShouldPlay(!shouldPlay)}
-          >
-            Play !
-          </button>
+        <button
+          type={'button'}
+          onClick={() => setShouldPlay(!shouldPlay)}
+        >
+          Play !
+        </button>
 
-        </div>
-        <div style={{fontSize: 60}}>
-          current speed: {entry ? (
-              <>
+      </div>
+      <div style={{ fontSize: 60 }}>
+        current speed: {entry ? (
+          <>
             {entry[0]} ({entry[1]} characters per second)
-              </>
-          ) : <>Please choose a speed</>
+          </>
+        ) : <>Please choose a speed</>
         }
         <Reader
           text={currentText}
           timeBetweenReads={timeBetweenReads}
           manualPlay={shouldPlay}
         />
-        </div>
-
       </div>
-    )
+
+    </div>
+  )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
-export const Header = () => {
+export const Header = ({ headerTitle = '' }) => {
   return (
     <header className="bg-black text-white p-4 text-center">
+      <b>{headerTitle}</b>
       <h1 className="text-3xl font-bold">ReadItOrDieTrying</h1>
       <p className="text-sm italic">No mercy. No hesitation. No excuses.</p>
     </header>
